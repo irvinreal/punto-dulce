e55728c6955d5dd7a7b79be99c470dd5ad46114b
irvinreal <irvin.glez.real@hotmail.com>
2023-11-26 13:20:25 -0600

    Despligue para mostrar al cliente

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
-import Header from '@/components/Header/Header'
+import Header from '../components/Header/Header'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
